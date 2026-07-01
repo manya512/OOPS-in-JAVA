bbd2a435b14f59c1f2ff5cc8faac89cba00298b2
Hemanya D <hemanya2510408@ssn.edu.in>
2026-07-01 22:39:50 +0530

    Add files via upload

---
+++ DataTypeDebug.java
@@ -0,0 +1,11 @@
+public class DataTypeDebug 
+{
+ public static void main(String[] args) 
+ {
+ long population = 8000000000L;   //large data
+ float price = 19.99f;             //without f, it is treated as double
+ String greeting = "Hello World!"; //declaration and initialisation comes first
+ System.out.println(greeting);
+ 
+ }
+}
